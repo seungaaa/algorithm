35886ccf011201a1ce1bf859c632143d443a8bea
seunga <tmddk224@naver.com>
2021-08-19 15:15:22 -0400

    algorithm#1

--- AlgorithmProblem/HashTable.cpp
+++ AlgorithmProblem/HashTable.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -6,11 +5,14 @@ using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
+    
+    sort(phone_book.begin(), phone_book.end());
+        
+        for(int i = 0 ; i < phone_book.size() - 1; i++)
+        {
+            if(phone_book[i] == phone_book[i+1].substr(0,phone_book[i].size()))
+                return false;
+        }
+    
     return answer;
 }
-
-int main(int argc, const char * argv[]) {
-    String list;
-    cin >> list;
-}
-
