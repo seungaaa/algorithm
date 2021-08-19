7e8d543977b997a6ea0cef2660f1107733888d31
seunga <tmddk224@naver.com>
2021-08-19 06:03:45 -0400

    codeup

---
+++ codeup_100/1018.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int h, m;
+    scanf("%d:%d", &h, &m);
+    cout << h << ":" << m;
+    return 0;
+}
