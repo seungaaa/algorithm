736e618928d1163210427ce90e5371b8f14cf6d4
hheeseung <ssigu69@gmail.com>
2021-08-19 11:10:27 +0900

    2주차 문법문제

---
+++ Week 2/Operation.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Operation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
---
+++ Week 2/Rest.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Rest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		System.out.println((a+b)%c);
+		System.out.println(((a%c)+(b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c)*(b%c))%c);
+
+	}
+
+}
