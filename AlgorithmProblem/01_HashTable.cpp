dee989cd9b7b6004cf80e6541b7a4977420eb12c
seunga <tmddk224@naver.com>
2021-08-21 21:23:04 -0400

    week2

--- AlgorithmProblem/01_HashTable.cpp
+++ AlgorithmProblem/01_HashTable.cpp
@@ -9,11 +9,11 @@ bool solution(vector<string> phone_book) {
     
     sort(phone_book.begin(), phone_book.end());
         
-        for(int i = 0 ; i < phone_book.size() - 1; i++)
-        {
-            if(phone_book[i] == phone_book[i+1].substr(0,phone_book[i].size()))
-                return false;
-        }
+    for(int i = 0 ; i < phone_book.size() - 1; i++)
+    {
+        if(phone_book[i] == phone_book[i+1].substr(0,phone_book[i].size()))
+            return false;
+    }
     
     return answer;
 }
