da08e02d23a49a2f2e25009134b5ac9ef7ee0ea6
seunga <tmddk224@naver.com>
2021-09-06 12:11:29 -0400

    week4

---
+++ CodeUp_100/basic_1035.cpp
@@ -0,0 +1,12 @@
+/*
+ 16진수로 입력된 정수 1개를 8진수로 바꾸어 출력해보자.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> hex >> n;
+    cout << oct << n;
+}
