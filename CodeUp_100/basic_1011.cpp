8b3fcca93471bb61d88914943d17255ed101beb3
seunga <tmddk224@naver.com>
2021-09-06 12:11:05 -0400

    week2

---
+++ CodeUp_100/basic_1011.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char x;
+    scanf("%c", &x);
+    printf("%c", x);
+    return 0;
+}
