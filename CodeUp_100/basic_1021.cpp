fd1231f53a71ada89944cef7d1ff9c7ca45fe525
seunga <tmddk224@naver.com>
2021-09-06 12:11:18 -0400

    week3

---
+++ CodeUp_100/basic_1021.cpp
@@ -0,0 +1,11 @@
+/*Informatics 출력*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char data[50] = "";
+    cin >> data;
+    cout << data;
+}
---
+++ CodeUp_100/basic_1028.cpp
@@ -0,0 +1,14 @@
+/*
+ 정수 1개를 입력받아 그대로 출력해보자.
+ (단, 입력되는 정수의 범위는 0 ~ 4,294,967,295 이다.)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    unsigned int n;
+    cin >> n;
+    cout << n;
+
+}
