775835a96c9147d0dabf6c68c98c56d7429b11d5
seunga <tmddk224@naver.com>
2021-09-21 01:30:37 -0400

    week1 CodeUp

---
+++ CodeUp_100/basic_1013.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a, b;
+    cin >> a >> b;
+    cout << a << " " << b;
+    return 0;
+}
