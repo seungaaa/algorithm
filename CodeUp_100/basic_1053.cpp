dae7e2eea4ab363b4d995adbfe3a3f7c53635e9a
seunga <tmddk224@naver.com>
2021-09-28 03:15:11 -0400

    week7

---
+++ CodeUp_100/basic_1053.cpp
@@ -0,0 +1,13 @@
+/*
+ 1(true, 참) 또는 0(false, 거짓)이 입력되었을 때
+ 반대로 출력하는 프로그램을 작성해보자.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    bool a;
+    cin >> a;
+    cout << !a;
+}
