065229b874607c33e8c7568552187948203a4c41
Selina <73357741+seungaaa@users.noreply.github.com>
2021-10-24 09:59:02 -0400

    week9 Refactor

--- AlgorithmPS/09_HashTable2.cpp
+++ AlgorithmPS/09_HashTable2.cpp
@@ -8,7 +8,7 @@ public:
     int lengthOfLongestSubstring(string s) {
         int answer = 0;
         unordered_map<char, int> charMap;
-        for (int i = 0, j = 0; i < s.size(); ++i) {
+        for (int i = 0, j = 0; i < s.size(); i++) {
             if (charMap.count(s[i]))
                 j = max(j, charMap[s[i]]+1);
             answer = max(answer, i-j+1);
